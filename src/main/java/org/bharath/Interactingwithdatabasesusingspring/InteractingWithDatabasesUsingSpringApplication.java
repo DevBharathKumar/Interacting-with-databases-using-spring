480195c8dd24c4b63e5e8612676288a9503c5663
Bharath Kumar <bharathk494@gmail.com>
2020-05-14 22:29:37 +0530

    Initial Commit

---
+++ src/main/java/org/bharath/Interactingwithdatabasesusingspring/InteractingWithDatabasesUsingSpringApplication.java
@@ -0,0 +1,13 @@
+package org.bharath.Interactingwithdatabasesusingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractingWithDatabasesUsingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractingWithDatabasesUsingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bharath/Interactingwithdatabasesusingspring/InteractingWithDatabasesUsingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bharath.Interactingwithdatabasesusingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractingWithDatabasesUsingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
