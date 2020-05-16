5c8c67c1b5b8caff44e17a52b9c91a8371018bfa
Bharath Kumar <bharathk494@gmail.com>
2020-05-16 11:28:13 +0530

    Converting the application from the H2 database to MySQL Database

--- src/main/java/org/bharath/Interactingwithdatabasesusingspring/MigeratingFromH2DatabaseToMySQLDatabase.java
+++ src/main/java/org/bharath/Interactingwithdatabasesusingspring/MigeratingFromH2DatabaseToMySQLDatabase.java
@@ -1,10 +1,8 @@
 package org.bharath.Interactingwithdatabasesusingspring;
 
-
 import java.util.Date;
 
 import org.bharath.Interactingwithdatabasesusingspring.entity.Person;
-import org.bharath.Interactingwithdatabasesusingspring.jpa.PersonJpaRepository;
 import org.bharath.Interactingwithdatabasesusingspring.springdatajpa.PersonSpringDataJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,34 +11,39 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Commenting the application for migrating from the H2 database to MySQL Database
-//@SpringBootApplication
-public class InteractingWithDatabasesUsingSpringDataJpa implements CommandLineRunner{
-
+@SpringBootApplication
+public class MigeratingFromH2DatabaseToMySQLDatabase implements CommandLineRunner
+{
 	@Autowired
 	PersonSpringDataJpaRepository repository;
 	
-	Logger logger = LoggerFactory.getLogger(InteractingWithDatabasesUsingSpringDataJpa.class);
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	public static void main(String[] args) {
-		SpringApplication.run(InteractingWithDatabasesUsingSpringDataJpa.class, args);
+		SpringApplication.run(MigeratingFromH2DatabaseToMySQLDatabase.class, args);
 	}
-
+	
 	@Override
 	public void run(String... args) throws Exception {
-		
-		logger.info("Getting details of a single person -> {} ",repository.findById(10003));
-		
-		logger.info("Inserting one person -> {} ",
-				repository.save(new Person("Abi", "Vandalur", new Date())));
-		
-		logger.info("Updating user 10003 -> {} ",
-				repository.save(new Person(10003,"Abdul","Zamin Pallavaram",new Date())));
 
-		repository.deleteById(10002);
-		
-		logger.info("All persons details -> {} ",repository.findAll());
-		
+		logger.info("Inserting one person -> {} ",
+		repository.save(new Person("Abi", "Vandalur", new Date())));
+
+		logger.info("Inserting one person -> {} ",
+		repository.save(new Person("Abdul","Pallavaram", new Date())));
+
+		logger.info("Inserting one person -> {} ",
+		repository.save(new Person("Rakesh","Pallavaram", new Date())));
+
+		logger.info("Updating user 10003 -> {} ",
+		repository.save(new Person(2,"Abdul","Zamin Pallavaram",new Date())));
+
+		logger.info("Getting details of a single person -> {} ",repository.findById(1));
+
+		repository.deleteById(1);
+
+		logger.info("All persons details -> {} ",repository.findAll());		
 	}
+
 
 }
